1c812f4f22fd703d686de4da4da8311cbbb15eda
Herroa <roman.gordov@mail.ru>
2023-02-18 18:58:11 +0700

    subtittles

--- sorts.c
+++ sorts.c
@@ -23,14 +23,14 @@ void printarray(int *array,int size)
         printf("%d\t",array[i]);
     printf("\n");
 }
-
+//print
 void swap(int *x, int *y)
 {
     int temp = *x;
     *x = *y;
     *y = temp;
 }
-
+//swap to insert
 void insertionsort(int *array,int size)
 {
     for(int i=0;i<size-1;i++){
@@ -74,6 +74,7 @@ void countingsort(int *array,int size)
     for (int i = 0; i < size; i++) {
     array[i] = output[i];
     }
+    //fill origin array
 }
 
 void merge(int arr[], int l, int m, int r)
@@ -82,19 +83,18 @@ void merge(int arr[], int l, int m, int r)
     int n1 = m - l + 1;
     int n2 = r - m;
   
-    /* create temp arrays */
     int L[n1], R[n2];
+    //left and right array
   
-    /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
+    //copy data
   
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    i = 0; 
+    j = 0;
+    k = l;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -106,34 +106,22 @@ void merge(int arr[], int l, int m, int r)
         }
         k++;
     }
-  
-    /* Copy the remaining elements of L[], if there
-    are any */
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-  
-    /* Copy the remaining elements of R[], if there
-    are any */
     while (j < n2) {
         arr[k] = R[j];
         j++;
         k++;
     }
 }
-  
-/* l is for left index and r is right index of the
-sub-array of arr to be sorted */
+
 void mergesort(int arr[], int l, int r)
 {
     if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
         int m = l + (r - l) / 2;
-  
-        // Sort first and second halves
         mergesort(arr, l, m);
         mergesort(arr, m + 1, r);
   
