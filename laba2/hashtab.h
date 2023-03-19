c4cba51f56352556aee6ee73f12368793670a42e
Herroa <roman.gordov@mail.ru>
2023-03-19 16:57:54 +0700

    hash

--- laba2/hashtab.h
+++ laba2/hashtab.h
@@ -4,14 +4,16 @@ typedef struct ht_item{
     char *value;
 } item;
 
-typedef struct hashtable{
-    item *items;
+typedef struct listnode{
+    item **items;
     int size;
     int count;
 } table;
 
 unsigned int hashtab_hash(char *key);
+
 void hashtab_init(struct listnode **hashtab);
+
 void hashtab_add(struct listnode **hashtab, char *key, int value);
 struct listnode *hashtab_lookup(struct listnode **hashtab, char *key);
 void hashtab_delete(struct listnode **hashtab, char *key);
