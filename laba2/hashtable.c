9ea845cce83f0b7fd4d9fd99f2da848b6324b6c4
Herroa <roman.gordov@mail.ru>
2023-03-29 20:26:41 +0700

    Hashtable lookup

--- laba2/hashtable.c
+++ laba2/hashtable.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include"hashtable.h"
 #define HASHTAB_SIZE 203
 
-unsigned int KRHash(char *s)
+unsigned int hashtab_hash(char *s)
 {
     unsigned int h = 0, hash_mul = 31;
     while (*s)
@@ -18,15 +20,43 @@ void hashtab_init(struct listnode **hashtab)
         hashtab[i] = NULL;
 }
 
+void hashtab_add(struct listnode **hashtab, char *key, int value)
+{
+    struct listnode *node;
 
+    int index = hashtab_hash(key);
+    node = malloc(sizeof(*node));
+    if (node != NULL) {
+        node->key = key;
+        node->value = value;
+        node->next = hashtab[index];
+        hashtab[index] = node;
+    }
+}
 
+struct listnode *hashtab_lookup(struct listnode **hashtab, char *key)
+{
+    struct listnode *node;
 
+    int index = hashtab_hash(key);
+    for (node = hashtab[index]; node != NULL; node = node->next) {
+        if (0 == strcmp(node->key, key))
+            return node;
+        }
+    return NULL;
+}
 
 
 int main()
 {
     struct listnode *hashtab[HASHTAB_SIZE];
-    unsigned int h = KRHash("ocelot");
-    printf("Hash sum: %d\n", h);
+    struct listnode *node;
+    hashtab_init(hashtab);
+    hashtab_add(hashtab, "Ocelot", 17);
+    hashtab_add(hashtab, "Flamingo", 4);
+    hashtab_add(hashtab, "Fox", 14);
+    node = hashtab_lookup(hashtab, "Flamingo");
+    if (node != NULL)
+    printf("Node: %s, %d\n", node->key, node->value);
     return 0;
 }
